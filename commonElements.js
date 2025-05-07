4417e95d2facf51b2a75044534ea57745fee9431
rajiul93 <rajiulislam283@gmail.com>
2025-05-07 08:53:08 +0600

    sum of array

--- commonElements.js
+++ commonElements.js
@@ -6,3 +6,9 @@ const commonElements = (array1, array2) => {
   return filter;
 };
 console.log("মিল থাকা উপাদান:", commonElements(arr1, arr2));
+
+const sumOfArray = (array) => {
+  return array.reduce((a, b) => a + b, 0);
+};
+
+console.log(sumOfArray(arr1));
