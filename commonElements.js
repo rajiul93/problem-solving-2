94684bf396afd09955df7deb4760a43c32fd23d0
rajiul93 <rajiulislam283@gmail.com>
2025-05-07 08:44:31 +0600

    common element find

---
+++ commonElements.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [4, 5, 6, 7, 8];
+
+const commonElements = (array1, array2) => {
+  const filter = array1.filter((e) => array2.includes(e));
+  return filter;
+};
+console.log("মিল থাকা উপাদান:", commonElements(arr1, arr2));
