01f95057ec97b11ff13162c3fe7e183943d7d170
rajiul93 <rajiulislam283@gmail.com>
2025-05-07 11:06:59 +0600

    reverse each word

---
+++ reverseEachWord.js
@@ -0,0 +1,10 @@
+const sentence = "A man is known by the company he keeps";
+
+const reverseEachWord = (string) => {
+  return string
+    .toLowerCase()
+    .split(" ")
+    .map((e) => e.split("").reverse().join(""))
+    .join(" ");
+};
+console.log(reverseEachWord(sentence));
